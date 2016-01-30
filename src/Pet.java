e7493010e5041f1b02d11f4f3364cde8242d0950
StewartWallace1115 <swallace3644@g.fmarion.edu>
2016-01-30 08:13:47 -0500

    Pet class
    Needs attributes and getter and setter methods for those attributes. 
    Also needs comments for the methods.

---
+++ src/Pet.java
@@ -0,0 +1,12 @@
+/*
+ * Name:  Jordan Powell, Stewart Wallace
+ * Date:  1/29/2016
+ * email: Jordan Powell   - 
+ *        Stewart Wallace - swallace3644@g.fmarion.edu
+ * 
+ * Purpose: This class models a real life pet. This class is a subclass of Animal.
+ * 			This class contains the animals cage number, and spayed or neurtered.
+ */
+public class Pet extends Animal{
+	
+}
