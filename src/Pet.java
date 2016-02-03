970577926c5ec224444261a275651370f47b90e2
StewartWallace1115 <swallace3644@g.fmarion.edu>
2016-02-03 17:53:44 -0500

    Pet
    Pet class attributes and methods( constructed - default and
    non-default-, getters and setters) were added.

--- src/Pet.java
+++ src/Pet.java
@@ -8,5 +8,34 @@
  * 			This class contains the animals cage number, and spayed or neurtered.
  */
 public class Pet extends Animal{
+	//CageCard cageCard = null;
+	boolean spayedNeutered;
+	String animalNumber;
+	
+	Pet(){
+		
+	}
+	
+	Pet(boolean spayedNeutered, String animalNumber){
+		this.animalNumber = animalNumber;
+		this.spayedNeutered = spayedNeutered;
+	}
+
+	public boolean isSpayedNeutered() {
+		return spayedNeutered;
+	}
+
+	public void setSpayedNeutered(boolean spayedNeutered) {
+		this.spayedNeutered = spayedNeutered;
+	}
+
+	public String getAnimalNumber() {
+		return animalNumber;
+	}
+
+	public void setAnimalNumber(String animalNumber) {
+		this.animalNumber = animalNumber;
+	}
+	
 	
 }
