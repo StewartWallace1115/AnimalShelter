29fa2a79b416f023c22faa241a4a1ec0e7358f8f
StewartWallace1115 <swallace3644@g.fmarion.edu>
2016-02-06 14:21:46 -0500

    Updated comments and attributes
    Finished the constructor. Updated comments for each method in the class.

--- src/Pet.java
+++ src/Pet.java
@@ -12,27 +12,49 @@ public class Pet extends Animal{
 	boolean spayedNeutered;
 	String animalNumber;
 	
+	/*
+	 * Default constructor the class. It sets all attributes to null or false for
+	 * boolean values. 
+	 */
 	Pet(){
-		
+		this.spayedNeutered = false;
+		this.animalNumber = null;
 	}
 	
+	/*
+	 * Initialization constructor. It initializes the class attributes with values 
+	 * provided as arguments for the constructor.
+	 */
 	Pet(boolean spayedNeutered, String animalNumber){
 		this.animalNumber = animalNumber;
 		this.spayedNeutered = spayedNeutered;
 	}
-
+	
+	/*
+	 * Returns a boolean value if the animal is either spayed or neutered. 
+	 */
 	public boolean isSpayedNeutered() {
 		return spayedNeutered;
 	}
-
+	
+	/*
+	 * Sets the boolean attribute spaytedNeutered. Accepts a boolean called 
+	 * spayedNeutered.
+	 */
 	public void setSpayedNeutered(boolean spayedNeutered) {
 		this.spayedNeutered = spayedNeutered;
 	}
-
+	
+	/*
+	 * Returns the animalNumber as a String.
+	 */
 	public String getAnimalNumber() {
 		return animalNumber;
 	}
-
+	
+	/*
+	 * Sets the animal's number. Accepts a String called animalNumber.
+	 */
 	public void setAnimalNumber(String animalNumber) {
 		this.animalNumber = animalNumber;
 	}
