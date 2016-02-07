4b0352f3a6e1e974dc97380f28121f0dfe1973e6
StewartWallace1115 <swallace3644@g.fmarion.edu>
2016-02-06 23:00:48 -0500

    Almost completed MainMenu for intake
    Needs AnimalIntakeForm to complete intake part of the class.

--- src/MainMenu.java
+++ src/MainMenu.java
@@ -17,7 +17,8 @@ import java.util.Scanner;
 //import javafx.scene.layout.*;
 public class MainMenu {
 	private int userInput;
-	
+	Pet pet = new Pet();
+	//AnimalIntakeForm intakeForm = new AnimalIntakeForm();
 	
 	MainMenu(){
 		this.userInput = 0;
@@ -41,6 +42,8 @@ public class MainMenu {
 					 flag = false;
 			case 2:  System.out.println("Animal intake");
 					 flag = false;
+					 animalIntakeMenu();
+					 break;
 			case 3:  System.out.println("Animal reclaimation");
 			         flag = false;
 			default: System.out.println(userInput + " wasn't an option");
@@ -51,13 +54,44 @@ public class MainMenu {
 		Scanner keyboard = new Scanner(System.in);
 		System.out.println("Enter owner, if known:");
 		String ownerName = keyboard.nextLine();
+		//intakeForm.setOwnerName();
 		
-		System.out.println("Enter where animal was located");
+		System.out.println("Enter where animal was located:");
 		String location = keyboard.nextLine();
+		//intakeForm.setLocation(location);
+		
+		System.out.println("Enter animal's name, if known:");
+		String name = keyboard.nextLine();
+		pet.setName(name);
 		
 		System.out.println("Enter animal's height");
 		String height = keyboard.nextLine();
+		pet.setHeight(height);
 		
+		System.out.println("Enter animal's eye color");
+		String eyeColor = keyboard.nextLine();
+		pet.setEyeColor(eyeColor);
 		
+		System.out.println("Enter animal's sex");
+		String sex = keyboard.nextLine();
+		pet.setSex(sex);
+		
+		System.out.println("Enter animal's weight");
+		String weight = keyboard.nextLine();
+		pet.setWeight(weight);
+		
+		System.out.println("Enter type of animal, i.e cat, dog:");
+		String animalType = keyboard.nextLine();
+		pet.setAnimalType(animalType);
+		
+		System.out.println("Enter animal's breed:");
+		String animalBreed = keyboard.nextLine();
+		pet.setAnimalBreed(animalBreed);
+		
+		System.out.println("Is the animal spayed or neutered? True or false");
+		boolean spayedNeutered = keyboard.nextBoolean();
+		pet.setSpayedNeutered(spayedNeutered);
+		
+		keyboard.close();
 	}
 }
