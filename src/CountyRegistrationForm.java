94cfb07d4dfdb1b0c6f10c3bb9917742b991c006
StewartWallace1115 <swallace3644@g.fmarion.edu>
2016-02-11 12:17:16 -0500

    CountyRegistrationForm
    Created CountyRegistrationForm

---
+++ src/CountyRegistrationForm.java
@@ -0,0 +1,62 @@
+import java.util.*;
+
+/*
+ * Name: Jordan Powell, Stewart Wallace
+ * Date: 2/8/2016
+ * email: 	jpowell3617@g.fmarion.edu
+ * 
+ * Purpose: This class holds the information for the County Registration Form. 
+ */
+
+public class CountyRegistrationForm 
+{
+	private int fee;
+	private Calendar dateRegistered;
+	private String tag;
+	private boolean certified;
+
+	public int getFee() 
+	{
+		return fee;
+	}
+
+	public void setFee(int fee) 
+	{
+		this.fee = fee;
+	}
+
+	public Calendar getDateRegistered() 
+	{
+		return dateRegistered;
+	}
+
+	public void setDateRegistered(Calendar dateRegistered) 
+	{
+		this.dateRegistered = dateRegistered;
+	}
+
+	public String getTag() 
+	{
+		return tag;
+	}
+
+	public void setTag(String tag) 
+	{
+		this.tag = tag;
+	}
+
+	public boolean isCertified()
+	{
+		return certified;
+	}
+	
+	/*public boolean isRegistrationComplete()
+	{
+		return this.certified;
+	}
+	
+	public void isRegistrationOutdated()
+	{
+		
+	}*/
+}
