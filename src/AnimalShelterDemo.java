cd16bb7f58cff27f8b564f918f74b2d6093e69ee
StewartWallace1115 <swallace3644@g.fmarion.edu>
2016-02-18 17:33:17 -0500

    Database connection
    Removed database connection and place it into separate class for
    boundary classes to use.

--- src/AnimalShelterDemo.java
+++ src/AnimalShelterDemo.java
@@ -5,28 +5,14 @@
  * 		
  * Purpose:This program implements the animal shelter project that was designed in CS313.
  */
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+
 
 public class AnimalShelterDemo {
 
 	public static void main(String[] args) {
-		try {
-		    @SuppressWarnings("unused")
-			Connection conn;
-		    conn = DriverManager.getConnection("jdbc:mysql://localhost:3306"
-		    		+ "/?user=root&password=cs313drive&useSSL=false");
 
-		    MainMenu mainMenu = new MainMenu();
-		    mainMenu.displayMenu();
-		  
-		} catch (SQLException ex) {
-		   
-		    System.out.println("SQLException: " + ex.getMessage());
-		    System.out.println("SQLState: " + ex.getSQLState());
-		    System.out.println("VendorError: " + ex.getErrorCode());
-		}
+	    MainMenu mainMenu = new MainMenu();
+	    mainMenu.displayMenu();
 
 
 	}
