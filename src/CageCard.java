84409b70e771ae38320f3b3240ab81fe4d4ff329
Stewart <Stewart@192.168.2.4>
2016-03-24 10:47:57 -0400

    CageCard Class
    CageCard Class created. Stores the information from the physical cage
    card.

---
+++ src/CageCard.java
@@ -0,0 +1,78 @@
+/*
+ * Name: Stewart Wallace, Jordan Powell
+ * Date: 3/15/2016
+ * email: swallace3644@g.fmarion.edu
+ * Purpose: This class models the real life cage card. It contains information about the 
+ * pet, the arrival data, cage number and when he or she is avalible for adoption.
+ */
+
+import java.util.Calendar;
+
+public class CageCard {
+	Pet pet;
+	Calendar arrivalDate;
+	int cageNumber;
+	Calendar dateAvalibleForAdoption;
+	
+	CageCard(){
+		pet = new Pet();
+		dateAvalibleForAdoption = Calendar.getInstance();
+		
+	}
+	
+	/*
+	 * Returns the pet object.
+	 */
+	public Pet getPet() {
+		return pet;
+	}
+	
+	/*
+	 * Sets the pet object. It takes in a Pet obejct as a parameter.
+	 */
+	public void setPet(Pet pet) {
+		this.pet = pet;
+	}
+	
+	/*
+	 * Returns a arrivalDate as Calendar data type.
+	 */
+	public Calendar getArrivalDate() {
+		return arrivalDate;
+	}
+	
+	/*
+	 * Sets the arrival data. The parameter is arrivalDatae of Calander type.
+	 */
+	public void setArrivalDate(Calendar arrivalDate) {
+		this.arrivalDate = arrivalDate;
+	}
+	
+	/*
+	 * Returns the cageNumber as an int.
+	 */
+	public int getCageNumber() {
+		return cageNumber;
+	}
+	
+	/*
+	 * Sets the cage number. It accepts a int parameter called cageNumber.
+	 */
+	public void setCageNumber(int cageNumber) {
+		this.cageNumber = cageNumber;
+	}
+	
+	/*
+	 * Returns the data avalible for adoption.
+	 */
+	public Calendar getDateAvalibleForAdoption() {
+		return dateAvalibleForAdoption;
+	}
+
+	/*
+	 * Sets the data avalible for adoption. It takes one parameter as Calender data type.
+	 */
+	public void setDateAvalibleForAdoption(Calendar dateAvalibleForAdoption) {
+		this.dateAvalibleForAdoption = dateAvalibleForAdoption;
+	}
+}
